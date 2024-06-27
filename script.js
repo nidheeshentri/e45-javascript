82cb17a648f581ae2777f3b0d9a78488c93eeabb
nidheeshentri <nidheesh.b@entri.me>
2024-06-27 20:36:16 +0530

    index.html file created

---
+++ script.js
@@ -0,0 +1,23 @@
+// Primitive
+// Reference
+
+
+// Number
+// String
+// Array
+
+let num = 10
+let num2 = num
+num = num + 1
+
+// 0, 1, 2, 3
+
+let students = ["S1", "S2", "S3", 30]
+let students2 = students
+
+students[0] = "s30"
+
+console.log(num)
+console.log(num2)
+console.log(students)
+console.log(students2)
